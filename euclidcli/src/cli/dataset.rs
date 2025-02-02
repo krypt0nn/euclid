32a0fbb8e85a14eb2ccdd09730a4deda34837b4d
Nikita Podvirnyi <krypt0nn@vk.com>
2025-02-02 13:34:03 +0200

    feat(cli): implement `dataset create` and `dataset insert`

--- euclidcli/src/cli/dataset.rs
+++ euclidcli/src/cli/dataset.rs
@@ -1,13 +1,44 @@
 use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
+use colorful::Colorful;
+
+use euclidlib::prelude::*;
 
 #[derive(Parser)]
 pub enum DatasetCli {
     /// Create new dataset.
     Create {
-        /// Path to the dataset file.
-        path: PathBuf
+        #[arg(long, short)]
+        /// Path to the database file.
+        path: PathBuf,
+
+        #[arg(long, default_value_t = -4096)]
+        /// SQLite database cache size.
+        ///
+        /// Positive value sets cache size in bytes, negative - in sqlite pages.
+        cache_size: i64
+    },
+
+    /// Insert document into the dataset.
+    Insert {
+        #[arg(long)]
+        /// Path to the database file.
+        database: PathBuf,
+
+        #[arg(long)]
+        /// Path to the document file.
+        document: PathBuf,
+
+        #[arg(long, default_value_t = String::new())]
+        /// Name of the document.
+        name: String,
+
+        #[arg(long, default_value_t = -4096)]
+        /// SQLite database cache size.
+        ///
+        /// Positive value sets cache size in bytes, negative - in sqlite pages.
+        cache_size: i64
     }
 }
 
@@ -15,9 +46,52 @@ impl DatasetCli {
     #[inline]
     pub fn execute(self) -> anyhow::Result<()> {
         match self {
-            Self::Create { path } => {
-                todo!()
+            Self::Create { path, cache_size } => {
+                match DocumentsDatabase::open(&path, cache_size) {
+                    Ok(_) => {
+                        let path = path.canonicalize().unwrap_or(path);
+
+                        println!("{}", format!("🚀 Database created in {path:?}").green());
+                        println!("{} {} command will create new database automatically if needed", "📖 Note:".blue(), "`dataset insert`".yellow());
+                    }
+
+                    Err(err) => eprintln!("{}", format!("🧯 Failed to create database: {err}").red())
+                }
+            }
+
+            Self::Insert { database, document, name, cache_size } => {
+                let database = database.canonicalize().unwrap_or(database);
+
+                println!("⏳ Opening database in {database:?}...");
+
+                match DocumentsDatabase::open(&database, cache_size) {
+                    Ok(database) => {
+                        let document = document.canonicalize().unwrap_or(document);
+
+                        println!("⏳ Reading document {document:?}...");
+
+                        match std::fs::read_to_string(document) {
+                            Ok(document) => {
+                                let document = Document::new(document)
+                                    .with_name(name);
+
+                                println!("⏳ Inserting document...");
+
+                                match database.insert(&document) {
+                                    Ok(_) => println!("{}", "✅ Document inserted".green()),
+                                    Err(err) => eprintln!("{}", format!("🧯 Failed to insert document: {err}").red())
+                                }
+                            }
+
+                            Err(err) => eprintln!("{}", format!("🧯 Failed to read document file: {err}").red())
+                        }
+                    }
+
+                    Err(err) => eprintln!("{}", format!("🧯 Failed to open database: {err}").red())
+                }
             }
         }
+
+        Ok(())
     }
 }
--- euclidcli/src/cli/mod.rs
+++ euclidcli/src/cli/mod.rs
@@ -1,1 +1,21 @@
 pub mod dataset;
+
+use clap::Parser;
+
+#[derive(Parser)]
+pub enum Cli {
+    /// Manage datasets of plain text corpuses.
+    Dataset {
+        #[command(subcommand)]
+        command: dataset::DatasetCli
+    }
+}
+
+impl Cli {
+    #[inline]
+    pub fn execute(self) -> anyhow::Result<()> {
+        match self {
+            Self::Dataset { command } => command.execute()
+        }
+    }
+}
