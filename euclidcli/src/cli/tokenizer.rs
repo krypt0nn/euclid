a7fbb7e6bc9f6ae27c13f76c53536e0c6c0d4d27
Nikita Podvirnyi <krypt0nn@vk.com>
2025-02-02 19:23:49 +0200

    feat(cli): added `tokenizer train` command

--- euclidcli/src/cli/tokenizer.rs
+++ euclidcli/src/cli/tokenizer.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+use std::sync::Mutex;
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -19,6 +21,48 @@ pub enum TokenizerCLI {
         #[arg(long)]
         /// Convert all the words to lowercase.
         lowercase: bool
+    },
+
+    /// Fit word embeddings model on the provided dataset.
+    Train {
+        #[arg(long)]
+        /// Path to the documents dataset database.
+        dataset: PathBuf,
+
+        #[arg(long)]
+        /// Convert all the words to lowercase.
+        lowercase: bool,
+
+        #[arg(long, default_value_t = 10)]
+        /// Amount of backpropagation timesteps to reach the target learn rate value.
+        ///
+        /// If set to 0 then no warmup will be applied.
+        warmup_duration: u32,
+
+        #[arg(long, default_value_t = 0.0015)]
+        /// Maximal difference from the target learn rate value.
+        ///
+        /// Actual learn rate will vary in range `[target - radius, target + radius]`.
+        ///
+        /// If set to 0 then no cyclic schedule will be applied.
+        cycle_radius: f32,
+
+        #[arg(long, default_value_t = 20)]
+        /// Amount of timesteps of backpropagation for going full cicle
+        /// of learning rate changing.
+        ///
+        /// If set to 0 then no cyclic schedule will be applied.
+        cycle_period: u32,
+
+        #[arg(long, default_value_t = 0.0015)]
+        /// Target learn rate of the backpropagation.
+        ///
+        /// It is different from the actual learn rate because we use cyclic
+        /// learn rate update schedule and warmup phase. During warmup learning
+        /// rate will slowly increase from `F::EPSILON` to the current value,
+        /// and then it will start going lower and higher according to the
+        /// cyclic schedule around the target value.
+        learn_rate: f32
     }
 }
 
@@ -40,10 +84,14 @@ impl TokenizerCLI {
                                 println!("⏳ Creating tiny word embeddings model...");
 
                                 let model = WordEmbeddingsModel::<f32>::random_tiny();
+                                let params = model.params();
 
-                                let (inputs, embeddings, params) = model.params();
+                                println!("{}", "✅ Model created".green());
+                                println!("              Input tokens: {}", format!("{}", params.input_tokens).yellow());
+                                println!("      Embedding dimensions: {}", format!("{}", params.embedding_dimensions).yellow());
+                                println!("  Embedding context radius: {}", format!("{}", params.embedding_context_radius).yellow());
+                                println!("                Parameters: {}", format!("{}", params.parameters).yellow());
 
-                                println!("✅ Model created: {inputs} inputs, {embeddings} embedding dimensions, {params} parameters");
                                 println!("⏳ Saving the model to the database...");
 
                                 match db.save_dynamic_model::<f32>(&model) {
@@ -100,7 +148,7 @@ impl TokenizerCLI {
                     }
                 };
 
-                let mut model_size = model.params().0 as i64;
+                let mut model_size = model.params().input_tokens as i64;
 
                 let parser = DocumentsParser::new(lowercase);
 
@@ -121,11 +169,16 @@ impl TokenizerCLI {
                                 None => anyhow::bail!("Failed to upscale word embeddings mdoel")
                             }
 
-                            let (new_inputs, new_embeddings, new_params) = model.params();
+                            let params = model.params();
 
-                            model_size = new_inputs as i64;
+                            model_size = params.input_tokens as i64;
 
-                            println!("✅ Model upscaled: {new_inputs} inputs, {new_embeddings} embedding dimensions, {new_params} parameters");
+                            println!("{}", "✅ Model upscaled".green());
+                            println!("              Input tokens: {}", format!("{}", params.input_tokens).yellow());
+                            println!("      Embedding dimensions: {}", format!("{}", params.embedding_dimensions).yellow());
+                            println!("  Embedding context radius: {}", format!("{}", params.embedding_context_radius).yellow());
+                            println!("                Parameters: {}", format!("{}", params.parameters).yellow());
+
                             println!("⏳ Saving updated model to the database...");
 
                             database.save_dynamic_model::<f32>(&model)?;
@@ -147,6 +200,127 @@ impl TokenizerCLI {
                     eprintln!("{}", format!("🧯 Failed to process documents from the dataset: {err}").red());
 
                     return Ok(());
+                }
+            }
+
+            Self::Train { dataset, lowercase, warmup_duration, cycle_radius, cycle_period, learn_rate } => {
+                let database = path.canonicalize().unwrap_or(path);
+                let dataset = dataset.canonicalize().unwrap_or(dataset);
+
+                println!("⏳ Opening tokenizer database in {database:?}...");
+
+                let database = match TokensDatabase::open(&database, cache_size) {
+                    Ok(database) => Rc::new(database),
+                    Err(err) => {
+                        eprintln!("{}", format!("🧯 Failed to open tokenizer database: {err}").red());
+
+                        return Ok(());
+                    }
+                };
+
+                println!("⏳ Opening documents dataset in {dataset:?}...");
+
+                let dataset = match DocumentsDatabase::open(&dataset, cache_size) {
+                    Ok(dataset) => dataset,
+                    Err(err) => {
+                        eprintln!("{}", format!("🧯 Failed to open documents dataset: {err}").red());
+
+                        return Ok(());
+                    }
+                };
+
+                println!("⏳ Loading word embeddings model...");
+
+                let model = match database.load_dynamic_model::<f32>() {
+                    Ok(Some(model)) => Rc::new(Mutex::new(model)),
+                    Ok(None) => Rc::new(Mutex::new(WordEmbeddingsModel::<f32>::random_tiny())),
+
+                    Err(err) => {
+                        eprintln!("{}", format!("🧯 Failed to load word embeddings model: {err}").red());
+
+                        return Ok(());
+                    }
+                };
+
+                let backpropagation = Rc::new(Mutex::new({
+                    Backpropagation::<{ GenericWordEmbeddingsModel::<1024, 32, f32>::PARAMS }, f32>::default()
+                        .with_warmup_duration(warmup_duration)
+                        .with_cycle_radius(cycle_radius)
+                        .with_cycle_period(cycle_period)
+                        .with_learn_rate(learn_rate)
+                }));
+
+                let parser = DocumentsParser::new(lowercase);
+
+                loop {
+                    let result = {
+                        let database = database.clone();
+                        let model = model.clone();
+                        let backpropagation = backpropagation.clone();
+
+                        dataset.for_each(move |i, document| {
+                            println!("⏳ Parsing document №{i}...");
+
+                            let now = std::time::Instant::now();
+
+                            let document = parser.parse(&document)
+                                .into_iter()
+                                .map(|token| {
+                                    match database.query_token(token) {
+                                        Ok(Some(token)) => Ok(Some(token as usize)),
+                                        Ok(None) => Ok(None),
+                                        Err(err) => Err(err)
+                                    }
+                                })
+                                .collect::<Result<Option<Vec<usize>>, _>>()?;
+
+                            let Some(document) = document else {
+                                anyhow::bail!("Some tokens of the document are not indexed");
+                            };
+
+                            println!("⏳ Training on {} parsed tokens...", document.len());
+
+                            backpropagation.lock().unwrap().timestep(|mut policy| {
+                                model.lock().unwrap().train::<{ GenericWordEmbeddingsModel::<1024, 32, f32>::PARAMS }>(&document, &mut policy);
+                            });
+
+                            let loss = model.lock().unwrap().total_loss(&document);
+
+                            println!("{}", format!("✅ Done after {:.1} seconds", now.elapsed().as_millis() as f64 / 1000.0).green());
+                            println!("     Min loss: {}", format!("{:.8}", loss.min_loss).yellow());
+                            println!("    Mean loss: {}", format!("{:.8}", loss.mean_loss).yellow());
+                            println!("     Max loss: {}", format!("{:.8}", loss.max_loss).yellow());
+                            println!("   Total loss: {}", format!("{:.8}", loss.total_loss).yellow());
+
+                            Ok(())
+                        })
+                    };
+
+                    if let Err(err) = result {
+                        eprintln!("{}", format!("🧯 Failed to process documents from the dataset: {err}").red());
+
+                        return Ok(());
+                    }
+
+                    let model = model.lock().unwrap();
+
+                    println!("⏳ Saving updated model to the database...");
+
+                    database.save_dynamic_model::<f32>(&model)?;
+
+                    println!("{}", "✅ Model saved".green());
+
+                    println!("⏳ Updating token embeddings...");
+
+                    let tokens = database.for_each(|token, _| {
+                        let embedding = model.encode(token as usize);
+
+                        database.insert_embedding::<f32>(token, &embedding)?;
+
+                        Ok(())
+                    })?;
+
+                    println!("{}", format!("✅ Updated {tokens} embeddings").green());
                 }
             }
         }
